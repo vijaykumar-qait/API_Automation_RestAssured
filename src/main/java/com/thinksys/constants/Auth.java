8a0268d9cf1ecb54392c69181c479b4df9b70f2f
amritpal93 <amrit7939@gmail.com>
2017-11-13 16:47:45 +0530

    REST_API_AUTOMATION

---
+++ src/main/java/com/thinksys/constants/Auth.java
@@ -0,0 +1,9 @@
+package com.thinksys.constants;
+
+public class Auth {
+
+	public static final String CONSUMER_KEY = "YoP9Pf2A5eyC8luOJMKZ0jFvE";
+	public static final String CONSUMER_SECRET = "zp9MpwUJZIseYbIJSBMKpQepRBTppUsvzUh3Z4iZ8w7vzTudZY";
+	public static final String ACCESS_TOKEN = "927824297002856448-PW2znCFbshR7o2laRTwR66GZZQK0vhc";
+	public static final String ACCESS_SECRET = "s3weXsEoTo3L2IO0tt7BzkJ0UyI31Ig0TzibUZRs1JSJP";
+}
---
+++ src/main/java/com/thinksys/constants/Path.java
@@ -0,0 +1,7 @@
+package com.thinksys.constants;
+
+public class Path {
+
+	public static final String BASE_URI = "https://api.twitter.com";
+	public static final String STATUSES = "/1.1/statuses";
+}
