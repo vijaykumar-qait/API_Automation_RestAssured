601a3754d687c2f021c9407425820268947c19a3
Vijay Kumar <vijay.kumar@VijayKumarMac.local>
2020-09-16 00:07:13 +0530

    Adding Create user and Add Pet API automation

--- src/main/java/com/thinksys/constants/Path.java
+++
@@ -1,7 +0,0 @@
-package com.thinksys.constants;
-
-public class Path {
-
-	public static final String BASE_URI = "https://api.twitter.com";
-	public static final String STATUSES = "/1.1/statuses";
-}
